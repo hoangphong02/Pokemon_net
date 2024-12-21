41e68e7744d8f566cc11f9ba995199166649bae7
hoangphong02 <hoangphongvl2002@gmail.com>
2024-12-21 15:04:34 +0700

    update models

---
+++ Pokemon/Models/Owner.cs
@@ -0,0 +1,31 @@
+﻿namespace Pokemon.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gym { get; set; }
+        public Country Country { get; set; }
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+
+    }
+}
+
+
+//public class City
+//{
+//    public int Id { get; set; }
+//    public string Name { get; set; }
+//    public int CountryId { get; set; } // Foreign key
+//    public Country Country { get; set; }
+//}
+
+//public class Country
+//{
+//    public int Id { get; set; }
+//    public string Name { get; set; }
+//    public ICollection<City> Cities { get; set; } // One-to-many relationship
+//}
+
+//City.Country: Đại diện cho mối quan hệ nhiều-một, nơi mỗi City thuộc về một Country.
+//Country.Cities: Đại diện cho mối quan hệ một-nhiều, nơi một Country có thể chứa nhiều City.
--- Pokemon/obj/Debug/net8.0/Pokemon.AssemblyInfo.cs
+++ Pokemon/obj/Debug/net8.0/Pokemon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a5a0bcd21bcf547ffa6e8ba8fb6fb9aa5ea77df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
